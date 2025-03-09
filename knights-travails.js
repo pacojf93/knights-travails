821eae837a11d316f67689522170f15af10d84cb
Paco <127772244+pacojf93@users.noreply.github.com>
2025-03-09 09:33:13 +0100

    added recursive method to get nodes in level order

--- knights-travails.js
+++ knights-travails.js
@@ -1,11 +1,12 @@
-const Square = (x, y) => {
-  const edges = [];
+const Square = (x, y, leavingSquare = null) => {
+  const enteringSquares = [];
   return {
     x,
     y,
+    leavingSquare,
     coordinates: () => [x, y],
-    addEdge: (square) => edges.push(square),
-    edges: () => edges,
+    addEnteringSquares: (square) => enteringSquares.push(square),
+    enteringSquares: () => enteringSquares,
   };
 };
 
@@ -19,6 +20,7 @@ const moves = [
   [-2, 1],
   [-1, 2],
 ];
+
 const getValidMoves = (x, y) =>
   moves.filter(
     (move) =>
@@ -36,16 +38,28 @@ const knightsMoves = (begin, end) => {
   const beginSquare = Square(begin[0], begin[1]);
   const endSquare = Square(end[0], end[1]);
 
-  (traverseBoard = function (square) {
-    const validMoves = getValidMoves(square.x, square.y);
-    validMoves.forEach((move) => console.log(move));
+  const level = (traverseBoard = function (queue, level) {
+    //search if any of the squares is the end square
+    for(let i = 0; i < queue.length; i ++) {
+        if((queue[i].x === endSquare.x) && (queue[i].y === endSquare.y)) return level
+    }
 
-    validMoves.forEach((move) =>
-        square.addEdge(Square(square.x + move[0], square.y + move[1]))
-    );
-    square.edges().forEach((edge) => console.log(`[${edge.x}, ${edge.y}]`));
-  })(beginSquare)
+    //get all the squares from the next level
+    let newQueue = []
+    queue.forEach(square => {
+        const validMoves = getValidMoves(square.x, square.y);
+        validMoves.forEach((move) =>
+            square.addEnteringSquares(Square(square.x + move[0], square.y + move[1]))
+        );
+        newQueue = [...newQueue, ...square.enteringSquares()]
+    })
+
+    return traverseBoard(newQueue, level + 1)
+
+  })([beginSquare], 0)
+
+  console.log(level)
 
 };
 
-knightsMoves([1, 1], [0, 0]);
+knightsMoves([1, 1], [3, 3]);
