982fb8fef745d2f99400cdb516eea0c2d4bdd260
Paco <127772244+pacojf93@users.noreply.github.com>
2025-03-09 10:26:14 +0100

    refactored code

--- knights-travails.js
+++ knights-travails.js
@@ -1,65 +1,76 @@
-const Square = (x, y, leavingSquare = null) => {
+const Square = (coordinates, leavingSquare = null) => {
+  const x = coordinates[0];
+  const y = coordinates[1];
   const enteringSquares = [];
+  const moves = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+  ];
+  const getValidMoves = (x, y) =>
+    moves.filter(
+      (move) =>
+        x + move[0] <= 7 &&
+        x + move[0] >= 0 &&
+        y + move[1] <= 7 &&
+        y + move[1] >= 0
+    );
   return {
+    coordinates,
+    leavingSquare,
     x,
     y,
-    leavingSquare,
-    coordinates: () => [x, y],
     addEnteringSquares: (square) => enteringSquares.push(square),
     enteringSquares: () => enteringSquares,
+    getValidMoves: () => getValidMoves(x, y),
+    toString: () => `[${x}, ${y}]`,
   };
 };
 
-const moves = [
-  [1, 2],
-  [2, 1],
-  [2, -1],
-  [1, -2],
-  [-1, -2],
-  [-2, -1],
-  [-2, 1],
-  [-1, 2],
-];
-
-const getValidMoves = (x, y) =>
-  moves.filter(
-    (move) =>
-      x + move[0] <= 7 &&
-      x + move[0] >= 0 &&
-      y + move[1] <= 7 &&
-      y + move[1] >= 0
-  );
-
-const printGraph = (root) => {
-  root.edges.forEach((edge) => console.log(`[${edge.x}, ${edge.y}]`));
-};
-
 const knightsMoves = (begin, end) => {
-  const beginSquare = Square(begin[0], begin[1]);
-  const endSquare = Square(end[0], end[1]);
+  const beginSquare = Square(begin);
+  const endSquare = Square(end);
 
-  const level = (traverseBoard = function (queue, level) {
-    //search if any of the squares is the end square
-    for(let i = 0; i < queue.length; i ++) {
-        if((queue[i].x === endSquare.x) && (queue[i].y === endSquare.y)) return level
+  //traverse the board in level order
+  const path = (traverseBoard = function (level) {
+    //get all the squares from the next level
+    let nextLevel = [];
+    level.forEach((square) => {
+      const validMoves = square.getValidMoves();
+      validMoves.forEach((move) =>
+        square.addEnteringSquares(
+          Square([square.x + move[0], square.y + move[1]], square)
+        )
+      );
+      nextLevel = [...nextLevel, ...square.enteringSquares()];
+    });
+
+    //check if any of the squares in level is the end square
+    for (var i in nextLevel) {
+      const square = nextLevel[i];
+      if (square.toString() === endSquare.toString()) {
+        //find the way to the top recursively
+        const path = (findWayBack = function (squares) {
+          return squares[0].leavingSquare === null
+            ? squares
+            : findWayBack([squares[0].leavingSquare, ...squares]);
+        })([square]);
+        return path;
+      }
     }
 
-    //get all the squares from the next level
-    let newQueue = []
-    queue.forEach(square => {
-        const validMoves = getValidMoves(square.x, square.y);
-        validMoves.forEach((move) =>
-            square.addEnteringSquares(Square(square.x + move[0], square.y + move[1]))
-        );
-        newQueue = [...newQueue, ...square.enteringSquares()]
-    })
+    return traverseBoard(nextLevel);
+  })([beginSquare], 0);
 
-    return traverseBoard(newQueue, level + 1)
+  console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+  path.forEach((square) => console.log(`[${square.x}, ${square.y}]`));
 
-  })([beginSquare], 0)
-
-  console.log(level)
-
+  return path;
 };
 
-knightsMoves([1, 1], [3, 3]);
+const squares = knightsMoves([3, 3], [4, 3]);
